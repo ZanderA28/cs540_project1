7fc835d361ac018232acb16ba0ab3b50f5526632
Alexander Asmussen <c697t652@wichita.edu>
2025-02-22 14:29:21 -0600

    Added required packages and libraries

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+import { Bar } from 'react-chartjs-2';
+import jsPDF from "jspdf";
+import "chart.js/auto";
 
 export default function Home() {
   return (
