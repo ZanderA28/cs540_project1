76ad7ebf565f56fed50941909511c13614e7b9ba
Alexander Asmussen <c697t652@wichita.edu>
2025-03-16 13:36:32 -0500

    Configured config file for static page export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+  reactStrictMode: true,
+  exportTrailingSlash: true, // This adds a trailing slash to the URL paths
+  basePath: '/your-repository-name',  // Optional, only if needed for GitHub Pages
+  assetPrefix: '/your-repository-name/', // Optional, only if needed for GitHub Pages
+};
