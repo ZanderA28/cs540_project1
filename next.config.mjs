73554ab3c27535c7ddc395e813e9f3ac02b5f019
Alexander Asmussen <c697t652@wichita.edu>
2025-03-16 13:55:38 -0500

    modify next.js workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
-module.exports = {
-  reactStrictMode: true,
-  exportTrailingSlash: true, // This adds a trailing slash to the URL paths
-  basePath: '/your-repository-name',  // Optional, only if needed for GitHub Pages
-  assetPrefix: '/your-repository-name/', // Optional, only if needed for GitHub Pages
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+  output: 'export',
 };
+
+export default nextConfig;
